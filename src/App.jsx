1d55c0840a9a2213fc0ae268ada65c5a31e5e6fd
katta saikrishna <katta_sai@nextazy.com>
2025-12-30 10:43:19 +0530

    Build issue Fix

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import BookingModal from "./components/BookingModal";
 import Stars from "./components/Stars";
-import Flower from "./components/FLower";
+import Flower from "./components/Flower";
 
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
